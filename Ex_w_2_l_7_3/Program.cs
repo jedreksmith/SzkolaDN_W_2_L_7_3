18120b1a731397386552e8a0d2b06f9c2fba37ca
Andrzej Kowalski <jedreksmith@poczta.onet.pl>
2024-05-12 22:05:17 +0200

    Add project files.

---
+++ Ex_w_2_l_7_3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.Design;
+
+namespace W_2_l_7_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            Console.WriteLine("Podaj liczbę");
+
+            a = int.Parse(Console.ReadLine());
+
+            if (a > 0)
+            {
+                Console.WriteLine(a + " to liczba dodatnia");
+            }
+            else
+            {
+                if (a == 0)
+                {
+                    Console.WriteLine(a + " to liczba zero");
+                }
+                else
+                {
+                    Console.WriteLine(a + " to liczba ujemna");
+                };
+            }
+        }
+    }
+
+}
